ef6e7db2a23eae725c24f415a645e721da887f08
suihan <suihan742@gmail.com>
2020-01-25 04:57:20 +0900

    add (temporary) sign-out button to the header

--- front/src/components/Header.vue
+++ front/src/components/Header.vue
@@ -1,19 +1,31 @@
 <template>
   <div id="header">
-    <h1 class="header-title">
+    <h1 id="header-title">
       <router-link to="/">
         <a>echo</a>
       </router-link>
     </h1>
+    <a id="sign-out" href="" @click="signOut">ログアウト</a>
   </div>
 </template>
 
 <script lang="ts">
+import firebase from 'firebase'
+
 export default {
   name: 'Header',
 
   props: {
     title: String
+  },
+
+  methods: {
+    signOut: function () {
+      firebase.auth().signOut().then(() => {
+        localStorage.removeItem('jwt')
+        this.$router.push('/signin')
+      })
+    }
   }
 }
 </script>
@@ -23,20 +35,34 @@ a {
   color: #eee;
   text-decoration: none;
 }
+a#sign-out:hover {
+  text-decoration: underline;
+}
 #header {
   background: #333;
-  width: 100vw;
+  width: 100%;
   height: 60px;
   margin-bottom: 40px;
   position: fixed;
   left: 0;
   top: 0;
+  display: flex;
+  flex-direction: row;
 }
-.header-title {
-  vertical-align: middle;
-  float: left;
+#header-title {
+  margin-top: auto;
+  margin-bottom: auto;
+  width: auto;
   padding: 0 20px;
   font-size: 22px;
-  color: #eee;
+  margin-right: auto;
+}
+#sign-out {
+  margin-top: auto;
+  margin-bottom: auto;
+  padding-right: 12px;
+  background: #00000000;
+  border: 0;
+  font-size: 8pt;
 }
 </style>
