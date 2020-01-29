a4f263ead3d232e24a0b358c90842ae3116cfd35
suihan <suihan742@gmail.com>
2020-01-30 01:27:11 +0900

    add webfont

--- front/src/App.vue
+++ front/src/App.vue
@@ -15,8 +15,11 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c&display=swap');
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'M PLUS Rounded 1c', sans-serif;
+/*  font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
